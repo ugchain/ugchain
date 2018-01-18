0a9672f40a55fcd2c57d94b526e2f19e28faaadf
Mr.Sofar <315520536@qq.com>
2018-01-18 18:20:25 +0800

    优化合约测试用例

--- test/UGChain.js
+++ test/UGChain.js
@@ -1,185 +1,200 @@
 var UGChain = artifacts.require("./UGChain.sol");
+var txId = "0xe3e391393728ad0957fe0c399067ea0d4ea38f0c93b501abe6d73edc78e7342a";
+var txId2 = "0xe3e391493728ad0957fe0c399067ea0d4ea38f0c93b501abe6d73edc78e7342a";
+var txId3 = "0xe3e301493728ad0957fe0c399067ea0d4ea38f0c93b501abe6d73edc78e7342a";
 
-const TOTAL_SUPPLY = 1000000000;
-// const TOTAL_SUPPLY = 1000000000;
-contract('UGChain', (accounts) => {
+
+/**
+ *  解冻(以太网络划转到联盟链)
+*/
+contract('defreeze', (accounts) => {
 	var initOwner = accounts[0];
-	var oneOwner = accounts[1];
-	var twoOwner = accounts[2];
-	var thrOwner = accounts[3];
-	var txId = "0xe3e391393728ad0957fe0c399067ea0d4ea38f0c93b501abe6d73edc78e7342a";
-	var txId2 = "0xe3e391493728ad0957fe0c399067ea0d4ea38f0c93b501abe6d73edc78e7342a";
-	var txId3 = "0xe3e301493728ad0957fe0c399067ea0d4ea38f0c93b501abe6d73edc78e7342a";
+	var oneAcc = accounts[1];
 
 
-/**
- * 存款操作 后面会删除 deposit 方法 
-*/
-	// it("deposit function",() => {
-	// 	var _instance;
-	// 	return UGChain.deployed()
-	// 		.then((instance) => {
-	// 			_instance = instance;
-	// 			//存款
-	// 			return Promise.all([
-	// 					_instance.deposit({value: toWei(2),from: initOwner})
-	// 				])
-	// 		})
-	// 		.then((data) => {
-	// 			setConsole("Deposit log",data);
-	// 			//合约余额
-	// 			return web3.eth.getBalance(UGChain.address)
-	// 		})
-	// 		.then((data) => {
-	// 			setConsole("Contract balance",fromWei(data));
-	// 			assert.equal(fromWei(data),2,"balance");
-	// 			//管理员存款
-	// 			return _instance.getOwnersDeposit(initOwner);
-	// 		})
-	// 		.then((data) => {
-	// 			setConsole("InitOwner balance",fromWei(data));
-	// 			assert.equal(fromWei(data),2,"balance");
-	// 		})
-	// })
-/**
- *  解冻
-*/
-	// it("defreeze", () => {
-	// 	var _instance;
-	// 	var twoBalance;
-	// 	var total = 0;
-	// 	return UGChain.deployed()
-	// 		.then((instance) => {
-	// 			_instance = instance;
-	// 			twoBalance = fromWei(web3.eth.getBalance(twoOwner));
-	// 			console.log(twoBalance);
-	// 			// 解冻：扣除管理员对应余额至 twoOwner 账户
-	// 			return _instance.defreeze(txId,twoOwner,{from:initOwner,value: toWei(5)});
-	// 		})
-	// 		.then((data) => {
-	// 			setConsole("defreeze twoOwner",data);
-	// 			var _logs = data.logs;
-	// 			//断言日志最后一次转账记录
-	// 			assert.equal(fromWei(_logs[_logs.length-1].args.amount),5,"To twoOwner balance");
-	// 			data.logs.forEach((v,i) => {
-	// 				console.log(v);
-	// 				total += fromWei(v.args.amount);
-	// 			})
-	// 			console.log(total);
-	// 			return web3.eth.getBalance(twoOwner);
-	// 		})
-	// 		.then((data) => {
-	// 			//twoOwner余额
-	// 			assert.equal(fromWei(data), total + twoBalance, "TwoOwner balance");
-	// 		})
-	// })
+
+	
+	it("defreeze", () => {
+		var _instance;
+		var onAccBalance;
+		var total = 0;
+		return UGChain.deployed()
+			.then((instance) => {
+				_instance = instance;
+				onAccBalance = fromWei(web3.eth.getBalance(oneAcc));
+				// 解冻：扣除管理员对应余额至 oneAcc 账户
+				return _instance.defreeze(txId,oneAcc,{from:initOwner,value: toWei(20)});
+			})
+			.then((data) => {
+				var _logs = data.logs;
+				//断言日志最后一次转账记录
+				assert.equal(fromWei(_logs[_logs.length-1].args.amount),20,"To oneAcc balance");
+				// 获取所有转账金额
+				data.logs.forEach((v,i) => {
+					if(v.event == 'Defreeze'){
+						total += fromWei(v.args.amount);
+					}
+				})
+				return web3.eth.getBalance(oneAcc);
+			})
+			.then((data) => {
+				//oneAcc余额
+				assert.equal(fromWei(data), total + onAccBalance, "oneAcc balance");
+			})
+	})
+});
 /* 
-	冻结
+	冻结(联盟链划转到以太)
 */
-	// it("freeze",() => {
-	// 		var _totalBalance;
-	// 		var _instance;
-	// 		var _value = 20;
-	// 		var _fee = 10;
-	// 	return UGChain.deployed()
-	// 		.then((instance) => {
-	// 			_instance = instance;
-	// 			// 获取合约余额
-	// 			return web3.eth.getBalance(UGChain.address)
-	// 		})
-	// 		.then((data) => {
-	// 			assert.equal(fromWei(data), 0, "Contract balance");
-	// 			// 发起冻结操作
-	// 			return _instance.freeze(toWei(_fee),{from: twoOwner,value: toWei(_value)});
-	// 		})
-	// 		.then((data) => {
-	// 			setConsole("freeze twoOwner",data);
-	// 			// 获取合约余额
-	// 			return web3.eth.getBalance(UGChain.address)
-	// 		})
-	// 		.then((data) => {
-
-	// 			assert.equal(fromWei(data), 20, "Contract balance");
-	// 			// 发起处理冻结操作
-	// 			return _instance.handleFreeze(twoOwner,initOwner);
-	// 		})
-	// 		.then((data) => {
-	// 			var _logs = data.logs;
-	// 			// 断言日志 转账数量手续费
-	// 			assert.equal(fromWei(_logs[_logs.length-1].args.value), _value, "freeze value");
-	// 			assert.equal(fromWei(_logs[_logs.length-1].args.fee), _fee, "freeze fee");
-	// 		})
-	// })
+contract('freeze', (accounts) => {
+	var initOwner = accounts[0];
+	var oneAcc = accounts[1];
+	it("freeze",() => {
+			var _totalBalance;
+			var _instance;
+			var _value = 20;
+			var _fee = 10;
+		return UGChain.deployed()
+			.then((instance) => {
+				_instance = instance;
+				// 获取合约余额
+				return web3.eth.getBalance(UGChain.address)
+			})
+			.then((data) => {
+				assert.equal(fromWei(data), 0, "Contract balance");
+				// 发起冻结操作
+				return _instance.freeze(toWei(_fee),{from: oneAcc,value: toWei(_value)});
+			})
+			.then((data) => {
+				// 获取合约余额
+				return web3.eth.getBalance(UGChain.address)
+			})
+			.then((data) => {
+				assert.equal(fromWei(data), 20, "Contract balance");
+				// 发起处理冻结操作
+				return _instance.handleFreeze(oneAcc,initOwner);
+			})
+			.then((data) => {
+				var _logs = data.logs;
+				// 断言日志 转账数量手续费
+				assert.equal(fromWei(_logs[_logs.length-1].args.value), _value, "freeze value");
+				assert.equal(fromWei(_logs[_logs.length-1].args.fee), _fee, "freeze fee");
+			})
+	})
+});
+	
 /**
  * defreezeByVote
 */
+contract('defreezeByVote', (accounts) => {
+	var initOwner = accounts[0];
+	var oneAcc = accounts[1];
+	var twoAcc = accounts[2];
 	it("defreezeByVote",() => {
 		var _instance;
 		return UGChain.deployed()
 			.then((instance) => {
 				_instance = instance;
-				setConsole("_instance",fromWei(web3.eth.getBalance(UGChain.address)));
-				return	_instance.deposit({value: toWei(30),from: thrOwner});
+				// 合约存款，保证合约货币数量不为空
+				return	_instance.deposit({value: toWei(30),from: twoAcc});
 			})
 			.then((data) => {
 				return web3.eth.getBalance(UGChain.address);
 			})
 			.then((data) => {
-				console.log(fromWei(data));
-				return _instance.defreezeByVote(txId2,twoOwner,toWei(5),{from: initOwner});
+				// 发起解冻余额投票
+				return _instance.defreezeByVote(txId2,oneAcc,toWei(5),{from: initOwner});
 			})
 			.then((data) => {
-				console.log(data);
 			 	return getOperstion(data);
 			})
 			.then((data) => {
+				// 确认投票当前状态
 				return _instance.hasConfirmed.call(data,initOwner)
 			})
 			.then((data) => {
-				console.log(data);
+				assert.equal(data,false,"vote status");
 			})
 	})
+})
+
 /**
  	returnToOwner
 */
-	// it("returnToOwner",() => {
-	// 	var _instance;
-	// 	return UGChain.deployed()
-	// 		.then((instance) => {
-	// 			_instance = instance;
-	// 			return _instance.defreeze(txId3,twoOwner,{from:initOwner,value: toWei(25)});
+contract('returnToOwner', (accounts) => {
+	var initOwner = accounts[0];
+	var oneAcc = accounts[1];
+	it("returnToOwner",() => {
+		var _instance;
+		return UGChain.deployed()
+			.then((instance) => {
+				_instance = instance;
+				// 管理员垫付
+				return _instance.defreeze(txId3,oneAcc,{from:initOwner,value: toWei(25)});
+			})
+			.then((data) => {
+				return Promise.all([
+						web3.eth.getBalance(initOwner),
+						_instance.getOwnersLoan(initOwner),
+						_instance.getOwnersDeposit(initOwner)
+					])
+			})
+			.then((data) => {
+				data.forEach((v,i) => {
+					console.log(fromWei(v));
+				})
+				// 申请报销
+				return _instance.returnToOwner(initOwner,toWei(20),{from: initOwner});
+			})
+			.then((data) => {
+				return Promise.all([
+					web3.eth.getBalance(initOwner),
+					_instance.getOwnersLoan(initOwner),
+					_instance.getOwnersDeposit(initOwner)
+				])
+			})
+			.then((data) => {
+				data.forEach((v,i) => {
+					console.log(fromWei(v));
+				})
+			})
+	})
+});
 
-	// 		})
-	// 		.then((data) => {
-	// 			return Promise.all([
-	// 					web3.eth.getBalance(initOwner),
-	// 					_instance.getOwnersLoan(initOwner),
-	// 					_instance.getOwnersDeposit(initOwner)
-	// 				])
-	// 		})
-	// 		.then((data) => {
-	// 			data.forEach((v,i) => {
-	// 				console.log(fromWei(v));
-	// 			})
-	// 			return _instance.returnToOwner(initOwner,toWei(20),{from: initOwner});
-	// 		})
-	// 		.then((data) => {
-	// 			setConsole("returnToOwner",data);
-	// 			return Promise.all([
-	// 				web3.eth.getBalance(initOwner),
-	// 				_instance.getOwnersLoan(initOwner),
-	// 				_instance.getOwnersDeposit(initOwner)
-	// 			])
-	// 		})
-	// 		.then((data) => {
-	// 			data.forEach((v,i) => {
-	// 				console.log(fromWei(v));
-	// 			})
-	// 		})
-	// })
-})
 
+/**
+ * 存款操作 后面会删除 deposit 方法 
+*/	
+// contract('returnToOwner', (accounts) => {
+// 	it("deposit function",() => {
+// 		var _instance;
+// 		return UGChain.deployed()
+// 			.then((instance) => {
+// 				_instance = instance;
+// 				//存款
+// 				return Promise.all([
+// 						_instance.deposit({value: toWei(2),from: initOwner})
+// 					])
+// 			})
+// 			.then((data) => {
+// 				setConsole("Deposit log",data);
+// 				//合约余额
+// 				return web3.eth.getBalance(UGChain.address)
+// 			})
+// 			.then((data) => {
+// 				setConsole("Contract balance",fromWei(data));
+// 				assert.equal(fromWei(data),2,"balance");
+// 				//管理员存款
+// 				return _instance.getOwnersDeposit(initOwner);
+// 			})
+// 			.then((data) => {
+// 				setConsole("InitOwner balance",fromWei(data));
+// 				assert.equal(fromWei(data),2,"balance");
+// 			})
+// 	})
+// })
 
+	
 
 
 
@@ -201,12 +216,12 @@ function getOperstion(res){
 	return Promise.resolve(operation);
 }
 
-function setConsole(title,result) {
-	console.log("==== " + title + " ====");
-	console.log(result);
-	console.log("==================== \r\n")
+// function setConsole(title,result) {
+// 	console.log("==== " + title + " ====");
+// 	console.log(result);
+// 	console.log("==================== \r\n")
 
-}
+// }
 function fromWei(val){
 	return web3.fromWei(val,"ether").toNumber();
 }
